7d202ab6428d7fa6c23f43aa54eb5a97696aa1cf
mosesgitonga <infosec947@gmail.com>
2024-01-04 21:19:00 +0300

    added  DOCSTRING

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This function generates a Pascal's Triangle up to the given number of rows.
+"""
+
 def pascal_triangle(n):
     if n <= 0:
         return []
@@ -8,7 +12,7 @@ def pascal_triangle(n):
         row = [1] * (i + 1)
         if i >= 2:
             for j in range(1, len(row) - 1):
-                row[j] = result[i - 1][j - 1] + result[i - 1][j]
+                row[j] = result[i - 1][j - 1] + result[i - 1][j - 1]
         result.append(row)
         
     return result
