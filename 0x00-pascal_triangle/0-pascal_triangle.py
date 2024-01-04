9d36dbc95426e133881091036adc5563b6d38ce9
mosesgitonga <infosec947@gmail.com>
2024-01-04 21:10:12 +0300

    correcting dir

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    result = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i >= 2:
+            for j in range(1, len(row) - 1):
+                row[j] = result[i - 1][j - 1] + result[i - 1][j]
+        result.append(row)
+        
+    return result
