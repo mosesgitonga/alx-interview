14d36ac9c1cee9d2aa4dba469e5ea43693b44461
mosesgitonga <infosec947@gmail.com>
2024-01-21 20:46:16 +0300

    fixed pycodetyling

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
+"""
+minimum operations
+"""
+
 
 def minOperations(n):
+    """
+    find minimum operations needed to result to n
+    """
+
     mylist = []
     while n != 1:
         for i in range(2, 9):
-            if n % i == 0: 
+            if n % i == 0:
                 n = n / i
                 mylist.append(i)
-    return sum(mylist )
+    return sum(mylist)
