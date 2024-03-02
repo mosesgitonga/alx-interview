4645fa1f4bb49bf78ee24af6ad061b591561fe35
mosesgitonga <infosec947@gmail.com>
2024-03-02 15:15:18 +0300

    finding change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    """
+    fewest number of coins to meet a given amount(total)
+    """
+    temp_total = total
+    perfectCoins = []
+    coins.sort(reverse=True)
+    for coin in coins:
+        while total >= coin:
+            if total - coin >= 0:
+                perfectCoins.append(coin)
+                pass
+            total -= coin
+
+    coin_sum = sum(perfectCoins)
+    if coin_sum == temp_total:
+        return len(perfectCoins)
+    else:
+        return -1
